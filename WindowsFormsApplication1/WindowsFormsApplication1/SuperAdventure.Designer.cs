c8768a24ee761404733fe4466df29059c3570c61
Kevin Do <kevindokhoale@gmail.com>
2016-09-14 16:49:18 -0700

    Basic Forms
    Player class with initial setup

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/SuperAdventure.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class SuperAdventure
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.lblGold = new System.Windows.Forms.Label();
+            this.lblHealth = new System.Windows.Forms.Label();
+            this.lblXP = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Health";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Level";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "XP";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AccessibleName = "";
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Location = new System.Drawing.Point(110, 19);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(0, 17);
+            this.lblLevel.TabIndex = 4;
+            this.lblLevel.UseMnemonic = false;
+            this.lblLevel.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lblGold
+            // 
+            this.lblGold.AutoSize = true;
+            this.lblGold.Location = new System.Drawing.Point(110, 99);
+            this.lblGold.Name = "lblGold";
+            this.lblGold.Size = new System.Drawing.Size(0, 17);
+            this.lblGold.TabIndex = 8;
+            // 
+            // lblHealth
+            // 
+            this.lblHealth.AutoSize = true;
+            this.lblHealth.Location = new System.Drawing.Point(110, 45);
+            this.lblHealth.Name = "lblHealth";
+            this.lblHealth.Size = new System.Drawing.Size(0, 17);
+            this.lblHealth.TabIndex = 7;
+            this.lblHealth.UseMnemonic = false;
+            // 
+            // lblXP
+            // 
+            this.lblXP.AutoSize = true;
+            this.lblXP.Location = new System.Drawing.Point(110, 73);
+            this.lblXP.Name = "lblXP";
+            this.lblXP.Size = new System.Drawing.Size(0, 17);
+            this.lblXP.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(18, 99);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 17);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Gold";
+            // 
+            // SuperAdventure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 524);
+            this.Controls.Add(this.lblGold);
+            this.Controls.Add(this.lblHealth);
+            this.Controls.Add(this.lblXP);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "SuperAdventure";
+            this.Text = "Wolly\'s Wild Ride";
+            this.Load += new System.EventHandler(this.SuperAdventure_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.Label lblGold;
+        private System.Windows.Forms.Label lblHealth;
+        private System.Windows.Forms.Label lblXP;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/SuperAdventure.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Engine;
+
+namespace WindowsFormsApplication1
+{
+    public partial class SuperAdventure : Form
+    {
+        private Player _player;
+
+        public SuperAdventure()
+        {
+            InitializeComponent();
+            _player = new Player();
+            _player.CurrentHitPoints = 100;
+            _player.MaximumHitPoints = 100;
+            _player.Gold = 5;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+
+            lblHealth.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblXP.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SuperAdventure_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
